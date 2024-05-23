32a44fc5f870df86a6e26f6991032089b165e4dd
ArtFrontEnd <artur.pavle@gmail.com>
2024-05-23 18:40:38 +0400

    change structure of scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,32 +3,21 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-// Task for building Sass styles
 function buildStyles() {
   return gulp.src('scss/style.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('css'));
 }
 
-// Task for watching Sass files for changes and running buildStyles and addPrefixes tasks
 async function watch() {
   const { default: autoprefixer } = await import('gulp-autoprefixer');
-
-  gulp.watch('scss/**/*.scss', gulp.series(buildStyles, addPrefixes(autoprefixer)));
+  gulp.watch('scss/**/*.scss', gulp.series(buildStyles, addPrefixes(autoprefixer))); // Call addPrefixes function here
 }
 
-// Task for adding prefixes to CSS files
 function addPrefixes(autoprefixer) {
-  return function () {
-    return gulp.src('scss/style.scss')
-      .pipe(sass.sync().on('error', sass.logError))
-      .pipe(autoprefixer())
-      .pipe(gulp.dest('css'));
-  };
+  return () => gulp.src('css/style.css') // Assuming this is the compiled CSS file
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('css'));
 }
 
-// Export tasks
-module.exports = {
-  buildStyles: buildStyles,
-  watch: watch
-};
+module.exports = { buildStyles, watch };
